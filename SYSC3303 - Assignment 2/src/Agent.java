971f8ac885459ed042acd247691f9873df5347ed
ryanribeiro <rmr@ryanribeiro.com>
2018-02-10 12:57:41 -0500

    Added getters and setters for Agent and Kitchen
    - Also modified constructors
    - Also added stuff to main() in Kitchen

--- SYSC3303 - Assignment 2/src/Agent.java
+++ SYSC3303 - Assignment 2/src/Agent.java
@@ -1,11 +1,39 @@
 
 public class Agent extends Thread{
 
-	public Agent () {
-		
+	private Ingredient ingredientOne, ingredientTwo, ingredientThree;
+	
+	public Agent (Ingredient one, Ingredient two, Ingredient three) {
+		this.setIngredientOne(one);
+		this.setIngredientTwo(two);
+		this.setIngredientThree(three);
 	}
 	
 	public void run() {
 		
+	}
+
+	public Ingredient getIngredientOne() {
+		return ingredientOne;
+	}
+
+	public void setIngredientOne(Ingredient ingredientOne) {
+		this.ingredientOne = ingredientOne;
+	}
+
+	public Ingredient getIngredientTwo() {
+		return ingredientTwo;
+	}
+
+	public void setIngredientTwo(Ingredient ingredientTwo) {
+		this.ingredientTwo = ingredientTwo;
+	}
+
+	public Ingredient getIngredientThree() {
+		return ingredientThree;
+	}
+
+	public void setIngredientThree(Ingredient ingredientThree) {
+		this.ingredientThree = ingredientThree;
 	}
 }
--- SYSC3303 - Assignment 2/src/Kitchen.java
+++ SYSC3303 - Assignment 2/src/Kitchen.java
@@ -1,11 +1,55 @@
 
 public class Kitchen {
 
-	public Kitchen () {
-		
+	private Agent agent;
+	private Chef chefOne, chefTwo, chefThree;
+	
+	public Kitchen (Agent agent, Chef one, Chef two, Chef three) {
+		this.setAgent(agent);
+		this.setChefOne(one);
+		this.setChefTwo(two);
+		this.setChefThree(three);
 	}
 	
 	public static void main(String[] args) {
+		Agent agent = new Agent(Ingredient.BREAD, Ingredient.JAM, Ingredient.BUTTER);
+		Chef chefOne = new Chef(Ingredient.BREAD);
+		Chef chefTwo = new Chef(Ingredient.JAM);
+		Chef chefThree = new Chef(Ingredient.BUTTER);
+		Kitchen kitchen = new Kitchen(agent, chefOne, chefTwo, chefThree);
 		
+		
+	}
+
+	public Agent getAgent() {
+		return agent;
+	}
+
+	public void setAgent(Agent agent) {
+		this.agent = agent;
+	}
+
+	public Chef getChefOne() {
+		return chefOne;
+	}
+
+	public void setChefOne(Chef chefOne) {
+		this.chefOne = chefOne;
+	}
+
+	public Chef getChefTwo() {
+		return chefTwo;
+	}
+
+	public void setChefTwo(Chef chefTwo) {
+		this.chefTwo = chefTwo;
+	}
+
+	public Chef getChefThree() {
+		return chefThree;
+	}
+
+	public void setChefThree(Chef chefThree) {
+		this.chefThree = chefThree;
 	}
 }
