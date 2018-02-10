04ae5e9305f841ad9b62adeb36cb26b3aef96f18
ryanribeiro <rmr@ryanribeiro.com>
2018-02-10 14:02:47 -0500

    Table class should be finished
    - Ability to put() ingredients on table (if it's not full)
    - Ability to see ingredients on table
    - Ability to take ingredients from the table
    - All methods synchronized

--- SYSC3303 - Assignment 2/src/Table.java
+++ SYSC3303 - Assignment 2/src/Table.java
@@ -4,30 +4,57 @@ public class Table {
 
 	private ArrayList<Ingredient> tableContents;
 	private boolean clear = true;
+	private boolean full = false;
 	
 	public Table () {
 		this.tableContents = new ArrayList<>();
 	}
 	
 	public synchronized void put(Ingredient ingredient) {
+		while (this.isFull()) {
+			try {
+				wait();
+			} catch (InterruptedException e){
+				System.err.println("Error occurred when trying to wait() after putting an ingredient on a full table.");
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
 		tableContents.add(ingredient);
+		if (tableContents.size() == 2)
+			this.setFull(true);
 		this.setClear(false);
 	}
 	
-	public void clearTable() {
+	public synchronized void clearTable() {
 		tableContents.clear();
 		this.setClear(true);
+		this.setFull(false);
 	}
 	
 	public synchronized ArrayList<Ingredient> getTableContents() {
 		return tableContents;
 	}
+	
+	public synchronized ArrayList<Ingredient> takeTableContents() {
+		ArrayList<Ingredient> ingredients = this.getTableContents();
+		this.clearTable();
+		return ingredients;
+	}
 
-	public boolean isClear() {
+	public synchronized boolean isClear() {
 		return clear;
 	}
 
-	public void setClear(boolean clear) {
+	public synchronized void setClear(boolean clear) {
 		this.clear = clear;
+	}
+	
+	public synchronized boolean isFull() {
+		return full;
+	}
+
+	public synchronized void setFull(boolean full) {
+		this.full = full;
 	}
 }
